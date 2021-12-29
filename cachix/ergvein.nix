eb8e4188fd925e5f60da64512f5cd21482b469af
nazgull08 <nazgull08@protonmail.com>
2021-12-29 18:32:38 +0300

    cachix

---
+++ cachix/ergvein.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://ergvein.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "ergvein.cachix.org-1:/4ozQ5aQqWuZPfT6EC//HTba0q0TVBcJld2HeZgnL20="
+    ];
+  };
+}
+    
